996ab1334e0a452e45a030a9ff69ab8b844beed7
Artem Streltsov <artem@streltsov.dev>
2024-10-20 14:17:19 +0100

    add admin

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
@@ -16,6 +17,15 @@ func main() {
 	token := os.Getenv("BOT_TOKEN")
 	if token == "" {
 		log.Fatal("Переменная среды BOT_TOKEN не установлена")
+	}
+
+	adminIDStr := os.Getenv("ADMIN_CHAT_ID")
+	if adminIDStr == "" {
+		log.Fatal("Переменная среды ADMIN_CHAT_ID не установлена")
+	}
+	adminID, err := strconv.ParseInt(adminIDStr, 10, 64)
+	if err != nil {
+		log.Fatalf("ADMIN_CHAT_ID должен быть числом: %v", err)
 	}
 
 	bot, err := tgbotapi.NewBotAPI(token)
@@ -33,7 +43,15 @@ func main() {
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		chatID := update.Message.Chat.ID
+		isAdmin := chatID == adminID
+		var msg tgbotapi.MessageConfig
+
+		if isAdmin {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Вы администратор")
+		} else {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Вы пользователь")
+		}
 
 		if _, err := bot.Send(msg); err != nil {
 			log.Printf("Ошибка отправки сообщения: %v", err)
