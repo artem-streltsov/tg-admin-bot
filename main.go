6c29a445f3156826393e013439c49796ac702a8b
Artem Streltsov <artem@streltsov.dev>
2024-10-20 13:39:00 +0100

    init bot

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatal("BOT_TOKEN environment variable not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatalf("Failed to create bot: %v", err)
+	}
+
+	bot.Debug = false
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("Failed to send message: %v", err)
+		}
+	}
 }
